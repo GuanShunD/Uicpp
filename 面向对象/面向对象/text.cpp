83fc9551880ed91700baab86f0ec8be507e510af
GuanShunD <1102533790@qq.com>
2025-06-01 20:45:06 +0800

    Add files via upload

---
+++ 面向对象/面向对象/text.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<iostream>
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	Date(const Date& d)//Date d2(d1)//引用传参，一般加const，防止被修改 
+//	{
+//		cout << "Date(const Date& d)" << endl;
+//		this->_year = d._year;
+//		this->_month = d._month;
+//		this->_day = d._day;		 
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+
+//int main()
+//{
+//	Date d1(2024, 11, 14);
+//	Date d2(d1);
+//
+//	return 0;
+//}
+
+//void func1(Date d)
+//{
+//
+//}
+//
+//Date func2()
+//{
+//	Date d;
+//	return d;//不是直接返回d，返回的是d的拷贝，作为一个临时变量，因为出来会被销毁
+//}
+//int main()
+//{
+//	Date d1(2024, 11, 14);
+//	func1(d1);
+//	func2();
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
